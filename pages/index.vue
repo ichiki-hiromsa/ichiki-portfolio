c4ea5b2fd68eaccf9400abdd7715d92a5768cb1d
ichiki-hiromsa <70831210+ichiki-hiromsa@users.noreply.github.com>
2023-07-04 23:13:51 +0900

    変更分

---
+++ pages/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-row justify="center" align="center">
+    <v-col cols="12" sm="8" md="6">
+      <v-card>
+        <v-card-title class="headline">
+          市来のホームページへようこそ！まだ準備中でーす。
+        </v-card-title>
+        <v-card-text>
+          <p>Vuetify is a progressive Material Design component framework for Vue.js. It was designed to empower developers to create amazing applications.</p>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer />
+        </v-card-actions>
+      </v-card>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+
+export default {
+
+}
+</script>
